6be65887fb4b0402e25747dca1812ad924b2b814
Konstantin Astashkin <kostya.astashkin911@gmail.com>
2023-07-18 17:51:10 +0200

    working with data def, rewrited collect, now working better

--- collect.py
+++ collect.py
@@ -5,7 +5,7 @@ import time
 
 
 #collecting data def
-def collect(url, param, options):
+def collect(categ, url, param, options):
     
     #requests and bs4 connect
     def connect(url, params={}, opt={"proxy": {}}):
@@ -25,13 +25,29 @@ def collect(url, param, options):
             soup = connect(url, params=param, opt=options)
             print('connection successful')
             for i in soup.find_all('article'):
-                p = i.find_all('span')
                 one = []
-                one.append(i.find("a").text)
-                for o in p:
-                    d = o.text
-                    one.append(d)
-                one.append(i.find('a')['href'])
+                one.append(i.find("a").text) #name
+                one.append(i.find('span', {'class':'text-12 text-gray-500'}).text) #from
+                one.append(i.find('span', {'class':'text-14 text-gray-500'}).text) #adress
+                #trying to find square_metre, price
+                sqm_pr = i.find('div', {'class': 'mt-16 flex justify-between sm:mt-8 sm:block space-x-12 font-bold whitespace-nowrap'})
+                if not sqm_pr:
+                    sqm_pr = i.find('div', {'class': 'col-span-2 mt-16 flex justify-between sm:mt-4 sm:block space-x-12 font-bold whitespace-nowrap'})
+                if sqm_pr: #square_metre, price
+                    for spr in sqm_pr.find_all('span'): 
+                        one.append(spr.text)
+                #trying to find price_desc, r_type_amount
+                desq_type = i.find('div', {'class': 'text-12 text-gray-500 flex flex-col mt-4 sm:block sm:mt-8'})
+                if not desq_type:
+                    desq_type = i.find('div', {'class': 'text-12 text-gray-500 flex flex-col mt-4 sm:block'})
+                if desq_type: #price_desc, r_type_amount
+                    for am in desq_type.find_all('span'):
+                        if am.text != ' ∙ ':
+                            one.append(am.text)
+                visning = i.find('span', {'class':'inline-block px-8 py-4 text-12 border border-bluegray-300 rounded-full'})
+                if visning:
+                    one.append(visning.text) #Visning
+                one.append(i.find('a', {'class': 'sf-search-ad-link link link--dark hover:no-underline'})['href']) #link
                 alt.append(one)
             try:
                 link = soup.find('nav', {"class": 'pagination u-pb8 u-pt16'}).find_all('a')[-1]['href']
