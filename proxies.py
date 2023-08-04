a3c74beebae1a84f4cbe202c4f46baa40314c3a6
Konstantin Astashkin <kostya.astashkin911@gmail.com>
2023-08-04 19:26:03 +0200

    Changed proxy on new

--- proxies.py
+++ proxies.py
@@ -1,6 +1,6 @@
-
+#working from 02 aug 2023 on 3 month
 def proxies():
     options = {'proxy': {
-        'http': 'http://Qm5ZLm:tjaEeq@45.146.183.178:8000',
-        'https': 'http://Qm5ZLm:tjaEeq@45.146.183.178:8000'}}
+        'http': 'http://user129095:j2xl9p@62.182.172.206:8411',
+        'https': 'http://user129095:j2xl9p@62.182.172.206:8411'}}
     return options
